a8a01f4833b3380524f442633274b5848f852f9b
Aniket <aniketraj28042003@gmail.com>
2024-06-28 19:50:07 +0530

    added productList box

---
+++ src/components/Pages/ProductBox/ProductBox.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import "./ProductBox.css";
+
+function ProductBox({productImg, productPrice, productDesc}) {
+    return (
+        <div className="product-list-box" id="productList">
+            <Link className="product-item text-decoration-none d-inline-block">
+                <div className="product-img">
+                    <img src={productImg}/>
+                </div>
+                <div className="product-name text-center">{productDesc}</div>
+                <div className="product-price text-center">&#8377; {productPrice}</div>
+            </Link>
+        </div>
+    )
+}
+
+export default ProductBox;
